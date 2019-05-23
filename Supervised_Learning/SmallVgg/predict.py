924e7d1fae267c2ca484c932bb3bb30296a1ced6
Chad Paik <chad.paik@voyagerx.com>
2019-05-24 01:28:55 +0900

    Added README, reformatted code

--- Supervised_Learning/SmallVgg/predict.py
+++ Supervised_Learning/SmallVgg/predict.py
@@ -1,10 +1,9 @@
-from keras.models import load_model
 import argparse
-import pickle 
+import pickle
+
 import cv2 as cv
-from matplotlib import pyplot as plt 
-
-
+from keras.models import load_model
+from matplotlib import pyplot as plt
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to input image that is going to be classified")
@@ -15,7 +14,6 @@ ap.add_argument("-e", "--height", type=int, default=28, help="target spatial dim
 ap.add_argument("-f", "--flatten", type=int, default=-1, help="whether or not we should flatten the image")
 args = vars(ap.parse_args())
 
-
 image = cv.imread(args["image"])
 output = image.copy()
 image = cv.resize(image, (args["width"], args["height"]))
@@ -24,32 +22,29 @@ plt.imshow(image)
 plt.show()
 
 if args["flatten"] > 0:
-	image = image.flatten()
-	image = image.reshape((1,image.shape[0]))
+    image = image.flatten()
+    image = image.reshape((1, image.shape[0]))
 
 else:
-	image = image.reshape((1,image.shape[0], image.shape[1], image.shape[2]))
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
 
-
-image = image.astype("float")/255.0
+image = image.astype("float") / 255.0
 
 model = load_model(args["model"])
 lb = pickle.loads(open(args["label_bin"], "rb").read())
 
-#make a prediction on the image s
+# make a prediction on the image s
 preds = model.predict(image)
 print(preds)
 print(lb.classes_)
 
-#find the class index with the largest probability 
+# find the class index with the largest probability
 i = preds.argmax(axis=1)[0]
 label = lb.classes_[i]
 
-text = "{}: {:.2f}%".format(label, preds[0][i]*100)
-cv.putText(output, text, (10,30), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+text = "{}: {:.2f}%".format(label, preds[0][i] * 100)
+cv.putText(output, text, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
 plt.imshow(output)
 plt.show()
 # cv.waitKey(0)
-
-
