5327382622e1ce67db743109f0a4672824d107bb
Chad Paik <chad.paik@voyagerx.com>
2019-05-28 00:09:32 +0900

    deleted one line from generator

--- GAN/CWGAN-GP/network/generator.py
+++ GAN/CWGAN-GP/network/generator.py
@@ -7,7 +7,6 @@ from base.base_model import BaseModel
 class Generator(BaseModel):
     def define_model(self, model_name):
         z = Input(shape=(self.config.data.latent_dim+self.config.data.label_len,))
-        # z = Input(shape=(self.config.data.latent_dim,))
         x = Dense((7 * 7 * 128))(z)
         x = LeakyReLU(alpha=0.2)(x)
         x = BatchNormalization()(x)
